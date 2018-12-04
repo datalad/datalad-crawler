64fd1341303d2808a088dc4c6e4eb34dc9c0c800
Yaroslav Halchenko <debian@onerussian.com>
2018-12-04 15:00:01 -0500

    ENH: get/drop_data, aggregate superdataset

--- datalad_crawler/pipelines/gh.py
+++ datalad_crawler/pipelines/gh.py
@@ -16,6 +16,11 @@ import re
 
 from datalad.api import Dataset, install
 from datalad.support import path as op
+from datalad.utils import (
+    assure_bool,
+    assure_list_from_str,
+    updated
+)
 from datalad.downloaders.credentials import UserPassword
 
 # Possibly instantiate a logger if you would like to log
@@ -24,7 +29,10 @@ from logging import getLogger
 lgr = getLogger("datalad.crawler.pipelines.github")
 
 
-def pipeline(org=None, repo_type='sources', include='.*', exclude='--'):
+def pipeline(org=None,
+             repo_type='sources', include='.*', exclude='--',
+             metadata_nativetypes=None, aggregate=True,
+             get_data=False, drop_data=False):
     """Crawl github organization and install all matching repos as subdatasets
 
     Parameters
@@ -40,6 +48,15 @@ def pipeline(org=None, repo_type='sources', include='.*', exclude='--'):
       By default we exclude all repositories with '--' in the name which we
       assume defines the dataset boundary, so for `ds1--subds2` there should
       also be `ds` repository which would define the relationship to `subds2`
+    metadata_nativetypes: list, optional
+      List of metadata native types to define (locally within .git/config to
+      not change repository itself) for metadata aggregation
+    aggregate: bool, optional
+      Aggregate metadata after crawling a new dataset
+    get_data: bool, optional
+      Get data for a new dataset
+    drop_data: bool, optional
+      Drop data for a new dataset after being done
 
     Returns
     -------
@@ -47,10 +64,18 @@ def pipeline(org=None, repo_type='sources', include='.*', exclude='--'):
       pipeline
     """
     assert org, "Organization must be provided"
+    aggregate = assure_bool(aggregate)
+    get_data = assure_bool(get_data)
+    drop_data = assure_bool(drop_data)
+
     import github as gh
     # TODO: consider elevating that function to a "public" helper
     from datalad.distribution.create_sibling_github import _get_github_entity
     superds = Dataset('.')
+    if metadata_nativetypes:
+        metadata_nativetypes = assure_list_from_str(metadata_nativetypes, sep=',')
+
+    aggregate_later = []
     def crawl_github_org(data):
         assert list(data) == ['datalad_stats'], data
         cred = UserPassword('github')
@@ -74,28 +99,99 @@ def pipeline(org=None, repo_type='sources', include='.*', exclude='--'):
             if op.exists(dspath):
                 lgr.info("Skipping %s since already exists", name)
                 # although we could just do install, which would at least
-                # verify that url is the same
+                # verify that url is the same... to not try to aggregate
+                # etc, we will just skip for now
+                continue
+
             # TODO: all the recursive etc options
-            ds = superds.install(dspath, source=repo.clone_url)
+            try:
+                ds = superds.install(
+                    dspath, source=repo.clone_url, get_data=get_data,
+                    on_failure='continue'
+                )
+            except Exception as exc:
+                if all(f.get('action', '') == 'get' for f in exc.failed):
+                    lgr.warning(
+                        "We failed to obtain %d files, extracted metadata etc might be incomplete",
+                        len(exc.failed)
+                    )
+                    ds = Dataset(exc.failed[0]['refds'])
+                else:
+                    raise
+
+            if metadata_nativetypes:
+                lgr.info("Setting native metadata types to include %s",
+                         ", ".join(metadata_nativetypes))
+                nativetypes = ds.config.obtain('datalad.metadata.nativetype',
+                                               default=[])
+                for nativetype in metadata_nativetypes:
+                    if nativetype not in nativetypes:
+                        lgr.debug("Adding %s nativetype", nativetype)
+                        ds.config.add('datalad.metadata.nativetype', nativetype, where='local')
+                    else:
+                        lgr.debug("Not adding %s nativetype since already defined", nativetype)
             # if anyone down the line needs it
+            aggregate_later.append(dspath)
             yield {
                 'dataset': ds,
+                'superdataset': superds,
                 'dataset_path': dspath,
                 'dataset_name': name,
                 'dataset_url': repo.clone_url,
             }
 
+
+    def do_aggregate_metadata(data):
+        # For now just aggregate into superdataset, and assume it is always provided!
+        agg_out = data['superdataset'].aggregate_metadata(
+            data['dataset_path'],
+            recursive=True,  # I feel greedy
+            incremental=True,
+            update_mode='target'  # we aggregate into superdataset!
+        )
+        yield dict(data, aggregate_metadata_result=agg_out)
+
+    def do_drop_data(data):
+        lgr.info("Dropping all data for %(dataset)s", data)
+        # This should fail to drop annexed metadata since no copies
+        # would be known
+        ds = data['dataset']
+        drop_res = ds.drop('.')
+        # TODO: annex forget -- since initial crawl, should be perfectly
+        # fine and might be of assistance to reduce impact on .git size
+        ds.repo.gc(allow_background=False)
+        yield dict(data, drop_result=drop_res)
+
+    def do_superds_aggregate_metadata(data):
+        # nothing in data would be of use, and we know superds
+        yield dict(
+            data,
+            aggregate_metadata_super_result=superds.aggregate_metadata(
+                '.',
+                incremental=True,
+            )
+        )
+
     # annex = Annexificator(no_annex=True, allow_dirty=True)
     lgr.info("Creating a pipeline for github organization %s", org)
-    return [
-        crawl_github_org,
-        # annex.initiate_dataset(
-        #     # We must not modify them
-        #     # template="GitHub",  # TODO: should be git - nothing githubish there AFAIK
-        #     data_fields=['dataset'],
-        #     existing='skip'
-        # )
+    pipeline = [
+            crawl_github_org,
     ]
+
+    if aggregate:
+        pipeline.append(do_aggregate_metadata)
+
+    if drop_data:
+        pipeline.append(do_drop_data)
+
+    if aggregate:
+        # but also we need a final aggregate for super ds itself
+        pipeline = [
+            pipeline,
+            do_superds_aggregate_metadata
+        ]
+
+    return pipeline
 
 
 # # There is nothing github'ish in it
