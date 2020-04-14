f32035ecae483dc711fe31f0f9f9b1d73ef5e9bb
Yaroslav Halchenko <debian@onerussian.com>
2020-04-13 22:28:02 -0400

    ENH(TST): a very basic test for crawling a github organization

---
+++ datalad_crawler/pipelines/tests/test_gh.py
@@ -0,0 +1,33 @@
+from datalad.utils import chpwd
+
+from datalad.api import (
+    crawl,
+    crawl_init,
+    create,
+)
+from datalad.consts import GITHUB_LOGIN_URL
+from datalad.downloaders.tests.utils import get_test_providers
+
+from datalad.tests.utils import (
+    assert_false,
+    assert_greater,
+    skip_if_no_network,
+    with_tempfile,
+)
+
+
+@skip_if_no_network
+@with_tempfile
+def test_crawl(tempd):
+    get_test_providers(GITHUB_LOGIN_URL)
+    ds = create(tempd)
+    with chpwd(tempd):
+        crawl_init(
+            template='gh', save=True,
+            args={'org': 'datalad-collection-1', 'include': 'kaggle'}
+        )
+        crawl()
+    subdss = ds.subdatasets(fulfilled=True, result_xfm='datasets')
+    assert all('kaggle' in d.path for d in subdss)
+    assert_greater(len(subdss), 1)
+    assert_false(ds.repo.dirty)
